37dc1645a5250e4825fbc028624799dd586dbe30
A.P.W.S.Dilrukshi <warunishashikala2001@gmail.com>
2025-01-30 14:56:54 +0530

    connect db

--- inventryProject/Form1.Designer.cs
+++ inventryProject/Form1.Designer.cs
@@ -28,13 +28,268 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.productid = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.productType = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.productName = new System.Windows.Forms.TextBox();
+            this.productQuntity = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.productColor = new System.Windows.Forms.ComboBox();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.insertbtn = new System.Windows.Forms.Button();
+            this.updatebtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.searchbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(297, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inventory Management ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // productid
+            // 
+            this.productid.Location = new System.Drawing.Point(167, 102);
+            this.productid.Name = "productid";
+            this.productid.Size = new System.Drawing.Size(167, 20);
+            this.productid.TabIndex = 2;
+            this.productid.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Product Type";
+            // 
+            // productType
+            // 
+            this.productType.Location = new System.Drawing.Point(167, 147);
+            this.productType.Name = "productType";
+            this.productType.Size = new System.Drawing.Size(215, 20);
+            this.productType.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(555, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Product Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(555, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Product Quntity";
+            // 
+            // productName
+            // 
+            this.productName.Location = new System.Drawing.Point(710, 99);
+            this.productName.Name = "productName";
+            this.productName.Size = new System.Drawing.Size(215, 20);
+            this.productName.TabIndex = 8;
+            // 
+            // productQuntity
+            // 
+            this.productQuntity.Location = new System.Drawing.Point(710, 146);
+            this.productQuntity.Name = "productQuntity";
+            this.productQuntity.Size = new System.Drawing.Size(215, 20);
+            this.productQuntity.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(40, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Product Color";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(555, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 21);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Date";
+            // 
+            // productColor
+            // 
+            this.productColor.FormattingEnabled = true;
+            this.productColor.Items.AddRange(new object[] {
+            "Red",
+            "Black",
+            "White",
+            "Green",
+            "Yellow",
+            "Blue"});
+            this.productColor.Location = new System.Drawing.Point(167, 197);
+            this.productColor.Name = "productColor";
+            this.productColor.Size = new System.Drawing.Size(215, 21);
+            this.productColor.TabIndex = 12;
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(710, 197);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(215, 20);
+            this.date.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(101, 268);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(824, 220);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // insertbtn
+            // 
+            this.insertbtn.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold);
+            this.insertbtn.Location = new System.Drawing.Point(87, 230);
+            this.insertbtn.Name = "insertbtn";
+            this.insertbtn.Size = new System.Drawing.Size(75, 23);
+            this.insertbtn.TabIndex = 15;
+            this.insertbtn.Text = "Insert";
+            this.insertbtn.UseVisualStyleBackColor = true;
+            // 
+            // updatebtn
+            // 
+            this.updatebtn.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold);
+            this.updatebtn.Location = new System.Drawing.Point(447, 230);
+            this.updatebtn.Name = "updatebtn";
+            this.updatebtn.Size = new System.Drawing.Size(75, 23);
+            this.updatebtn.TabIndex = 16;
+            this.updatebtn.Text = "Update";
+            this.updatebtn.UseVisualStyleBackColor = true;
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deletebtn.Location = new System.Drawing.Point(828, 230);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(75, 23);
+            this.deletebtn.TabIndex = 17;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            // 
+            // searchbtn
+            // 
+            this.searchbtn.Location = new System.Drawing.Point(353, 99);
+            this.searchbtn.Name = "searchbtn";
+            this.searchbtn.Size = new System.Drawing.Size(75, 23);
+            this.searchbtn.TabIndex = 18;
+            this.searchbtn.Text = "Search";
+            this.searchbtn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(923, 525);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(1060, 655);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.searchbtn);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.updatebtn);
+            this.Controls.Add(this.insertbtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.productColor);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.productQuntity);
+            this.Controls.Add(this.productName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.productType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.productid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox productid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox productType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox productName;
+        private System.Windows.Forms.TextBox productQuntity;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox productColor;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button insertbtn;
+        private System.Windows.Forms.Button updatebtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.Button searchbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- inventryProject/Form1.cs
+++ inventryProject/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,22 @@ namespace inventryProject
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-9L1GMLB;Initial Catalog=inventory;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
